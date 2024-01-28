d347ee4c22b0a1fc24b42039c5b714f035ca3c85
Raviteja Tanikella <91713140+TVR28@users.noreply.github.com>
2024-01-27 22:48:31 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ enableCORS=false\n\
 port = $PORT\n\
 [theme]\n\
 primaryColor = '#7393B3'\n\
-backgroundColor = '#808080'\n\
-secondaryBackgroundColor = '#FFFFFF'\n\
+backgroundColor = '#d3d3d3'\n\
+secondaryBackgroundColor = '#ededed'\n\
 textColor = '#000000'\n\
 " > ~/.streamlit/config.toml
